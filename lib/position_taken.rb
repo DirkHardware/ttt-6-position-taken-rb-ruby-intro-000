d21fc18e0dd805ee76d1b74ffb0ac13895faa8ed
Anderson Rolf <email.the.anderson@gmail.com>
2019-04-30 21:08:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,24 +1,26 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
-# def position_taken?(board, index)
-#   if board[index] == " " || ""
-#     return false
-#   elsif board[index] == "X" || "O"
-#     return true
-#   end
-# end
-
-
 def position_taken?(board, index)
-  if board[index] == "X"
+  if board[index] == " " || ""
+    return false
+  elsif board[index] == nil 
+    return false 
+  elsif board[index] == "X" || "O"
     return true
-  elsif board[index] == "O"
-    return true
-  elsif board[index] == " "
-    return false
-  elsif board[index] == ""
-    return false
-  elsif board[index] == nil
-    return false
   end
 end
+
+
+# def position_taken?(board, index)
+#   if board[index] == "X"
+#     return true
+#   elsif board[index] == "O"
+#     return true
+#   elsif board[index] == " "
+#     return false
+#   elsif board[index] == ""
+#     return false
+#   elsif board[index] == nil
+#     return false
+#   end
+# end
