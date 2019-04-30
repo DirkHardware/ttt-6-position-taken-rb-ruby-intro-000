bd9ecd2dee5fcac317bb609cb35d57ddf98ee8ad
Anderson Rolf <email.the.anderson@gmail.com>
2019-04-30 20:35:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 index = 3
 
 def position_taken?(board, index)
-  elsif board[index] == "X" || "O"
+  if board[index] == "X" || "O"
     return true
   elsif board[index] == " " || ""
     return false
